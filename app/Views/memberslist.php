ec61a02e9bf9190a7c0a5571a33855896f1c2d02
PeicongZhan <972260627@qq.com>
2020-11-01 22:09:50 +0800

    Show the list of name

---
+++ app/Views/memberslist.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Lakers championship !!!</title>
+	</head>
+	<body>
+		<h1>Six core members</h1>
+		{records}
+		<p><a href="/namelist/showme/{id}">{name}</a></p>
+		{/records}
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
