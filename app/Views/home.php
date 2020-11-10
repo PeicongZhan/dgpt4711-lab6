020131baafd4e4ff6a92554920bd131ae9c3679c
PeicongZhan <44920467+PeicongZhan@users.noreply.github.com>
2020-11-10 15:34:55 +0800

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Lakers championship !!!</title>
+		<title>DGPT4711 Lab 06</title>
 	</head>
 	<body>
-		<h1>Lakers championship !!!</h1>
-		<p><a href="/namelist">Six core members info</a>.</p>
-		<p><a href="/playersdata">Show the players data</a></p>
+		<h1>Travel Destinations</h1>
+		<p><a href="/travel">Show the destinations</a>.</p>
+		<p><a href="/places">Show the places raw data</a></p>
 	</body>
 </html>
